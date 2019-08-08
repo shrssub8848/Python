374a3614b011e713a199572207d5498c643ec051
shuv <shrssub8848@gmail.com>
2019-08-07 23:57:35 -0700

    models

--- Newproject/NewprojectP/home/admin.py
+++ Newproject/NewprojectP/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from home.models import Introduction, Python, Student1
 # Register your models here.
+admin.site.register(Introduction)
+admin.site.register(Python)
+admin.site.register(Student1)
--- Newproject/NewprojectP/home/models.py
+++ Newproject/NewprojectP/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Introduction(models.Model):
+	name= models.CharField(max_length=50,unique=True)# need memory for string not for integer
+	age = models.IntegerField()
+	number = models.IntegerField()
+
+
+class Python(models.Model):
+	Session = models.CharField(max_length=20,unique=True)
+
+
+class Student1(models.Model):
+	name = models.CharField(max_length=20,unique=False)
+	Session = models.ForeignKey("Python", on_delete=models.PROTECT)
