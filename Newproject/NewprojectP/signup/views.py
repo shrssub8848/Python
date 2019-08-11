edfe4d071246ae8e462f00b8eb29df3bd700738f
shuv <shrssub8848@gmail.com>
2019-08-11 16:22:50 -0700

    csrf to add encryption

--- Newproject/NewprojectP/signup/views.py
+++ Newproject/NewprojectP/signup/views.py
@@ -9,5 +9,7 @@ def signup(request):
     if request.method == "POST":
         form = forms.FormName(request.POST)
         if form.is_valid():
+            print("name ="+form.cleaned_data["email"])#print email on console
+            print(request.POST.get("name"))#print name on console
             form.save()
     return render(request, 'home/signup.html', {'signup':form})
